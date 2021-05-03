0102f4d85d16d1f9f7fede9f927544a346b09ff6
Nigel0583 <43179051+Nigel0583@users.noreply.github.com>
2021-05-03 15:09:17 +0100

    change img

--- src/components/comms/Comment.js
+++ src/components/comms/Comment.js
@@ -6,7 +6,7 @@ class Comment extends Component {
             <article className="media">
                 <figure className="media-left">
                     <p className="image is-64x64">
-                        <img src="https://bulma.io/images/placeholders/128x128.png" alt="Avatar" />
+                        <img src="./assets/img/f1_helmet.svg" alt="Avatar" />
                     </p>
                 </figure>
                 <div className="media-content">
